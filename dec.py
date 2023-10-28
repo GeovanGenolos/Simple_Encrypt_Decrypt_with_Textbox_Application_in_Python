f1da5f9ca9df4f937419ad5f2fa9ad18a81e1d35
Geovan Genolos <142429957+GeovanGenolos@users.noreply.github.com>
2023-10-28 18:09:48 +0800

    Update dec.py

--- dec.py
+++ dec.py
@@ -1,25 +1,25 @@
 import tkinter as tk
 
-# Function to perform a simple decryption
+# Function to perform a simple decryption.
 def decrypt_text(text):
     decrypted_text = ""
     for char in text:
-        decrypted_text += chr(ord(char) - 1)  # Shifting each character back by 1 ASCII value
+        decrypted_text += chr(ord(char) - 1)  # Shifting each character back by 1 ASCII value.
     return decrypted_text
 
-# Function to handle button click and decrypt the input
+# Function to handle button click and decrypt the input.
 def decrypt_input():
     encrypted_text = input_entry.get()
     decrypted_text = decrypt_text(encrypted_text)
     output_label.config(text=decrypted_text)
-    output_text.delete('1.0', tk.END)  # Clear the text box
-    output_text.insert(tk.END, decrypted_text)  # Insert decrypted text into the text box
+    output_text.delete('1.0', tk.END)  # Clear the text box.
+    output_text.insert(tk.END, decrypted_text)  # Insert decrypted text into the text box.
 
-# Creating the main window
+# Creating the main window.
 window = tk.Tk()
 window.title("Decryption Generator")
 
-# Creating GUI components
+# Creating GUI components.
 input_label = tk.Label(window, text="Enter text to decrypt:")
 input_label.pack()
 
@@ -35,5 +35,4 @@ output_label.pack()
 output_text = tk.Text(window, height=1, width=30)
 output_text.pack()
 
-# Running the main loop
 window.mainloop()
