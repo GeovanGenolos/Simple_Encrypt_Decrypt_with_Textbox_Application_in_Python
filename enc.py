843b858f523d366bd0940c6636e19aaf94c5363f
Geovan Genolos <142429957+GeovanGenolos@users.noreply.github.com>
2023-10-28 18:10:35 +0800

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,25 +1,25 @@
 import tkinter as tk
 
-# Function to perform a simple encryption
+# Function to perform a simple encryption.
 def encrypt_text(text):
     encrypted_text = ""
     for char in text:
-        encrypted_text += chr(ord(char) + 1)  # Shifting each character by 1 ASCII value
+        encrypted_text += chr(ord(char) + 1)  # Shifting each character by 1 ASCII value.
     return encrypted_text
 
-# Function to handle button click and encrypt the input
+# Function to handle button click and encrypt the input.
 def encrypt_input():
     input_text = input_entry.get()
     encrypted_text = encrypt_text(input_text)
     output_label.config(text=encrypted_text)
-    output_text.delete('1.0', tk.END)  # Clear the text box
-    output_text.insert(tk.END, encrypted_text)  # Insert encrypted text into the text box
+    output_text.delete('1.0', tk.END)  # Clear the text box.
+    output_text.insert(tk.END, encrypted_text)  # Insert encrypted text into the text box.
 
-# Creating the main window
+# Creating the main window.
 window = tk.Tk()
 window.title("Encryption Generator")
 
-# Creating GUI components
+# Creating GUI components.
 input_label = tk.Label(window, text="Enter text to encrypt:")
 input_label.pack()
 
@@ -35,5 +35,4 @@ output_label.pack()
 output_text = tk.Text(window, height=1, width=30)
 output_text.pack()
 
-# Running the main loop
 window.mainloop()
